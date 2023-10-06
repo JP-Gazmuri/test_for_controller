c377aca34afacd0538b71c28301122f6ef2683d1
webtech <webtech@webtech-debian.webtech>
2023-10-05 22:44:37 -0300

    first v

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,16 @@
+class TestsController < ApplicationController
+
+    def show
+      number = params[:id]
+      number2 = rand(1..100)
+      data = { url_number: number.to_s, random_number: number2.to_s }
+
+      respond_to do |format|
+        format.json { render json: data }
+      end
+    end
+
+    def index
+      
+    end
+end
